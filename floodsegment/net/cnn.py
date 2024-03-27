7aeaa5957341c0665e8bdbb9757595eec8073273
Neeth Kunnath <neeth.xavier@gmail.com>
2024-03-27 19:39:44 -0400

    wip: cnn

--- floodsegment/net/cnn.py
+++ floodsegment/net/cnn.py
@@ -1,7 +1,8 @@
 import torch.nn as nn
 
 from floodsegment.utils.builder import build_object
-from floodsegment.net.module import GenericBlock
+from floodsegment.net.base import _Buildable
+from floodsegment.net.module import GenericBlock, SimpleConvLayer
 from typing import Any, Dict, List
 
 import logging
@@ -25,16 +26,24 @@ class BaseCnn(nn.Module):
         # self.decoder = build_object(**decoder)
 
 
-class GenericCnn(nn.Module):
+class GenericCnn(_Buildable):
     def __init__(
         self,
+        *,
+        # base layer params
         input_ch: int,
+        init_kernel_size: int,
+        init_stride: int,
+        init_activation: Dict[str, Any],
+        init_normalization: Dict[str, Any],
+        # Block params
         layer_config: List[int],
         channel_config: List[int],
         stride_config: List[int],
         # dilation_config: List[int],
         base_config: Dict[str, Any],
         net_name: str,
+        **kwargs,
     ):
         if not channel_config:
             assert len(layer_config) == len(channel_config), f"channel config must be the same as layer config"
@@ -43,30 +52,66 @@ class GenericCnn(nn.Module):
         # if not dilation_config:
         #    assert len(layer_config) == len(dilation_config), f"dilation config must be the same as layer config"
 
-        super(__class__, self).__init__()
+        super(__class__, self).__init__(
+            input_ch=input_ch,
+            init_kernel_size=init_kernel_size,
+            init_stride=init_stride,
+            init_activation=init_activation,
+            init_normalization=init_normalization,
+            layer_config=layer_config,
+            channel_config=channel_config,
+            stride_config=stride_config,
+            base_config=base_config,
+            **kwargs,
+        )
 
         self.net_name = net_name
         self.input_ch = input_ch
         self.layer_config = layer_config
         self.channel_config = channel_config
         self.stride_config = stride_config
-        self.base_config: nn.Module = base_config
+        self.base_config = base_config
 
-        self.block = nn.ModuleList()
-        self._build()  # populate self.block
-
-    def _build(self) -> nn.ModuleList:
+    def _build(
+        self,
+        *,
+        input_ch: int,
+        init_kernel_size: int,
+        init_stride: int,
+        init_activation: Dict[str, Any],
+        init_normalization: Dict[str, Any],
+        layer_config: List[int],
+        channel_config: List[int],
+        stride_config: List[int],
+        base_config: Dict[str, Any],
+        **kwargs,
+    ) -> nn.ModuleList:
         out_ch = None
-        for i in range(len(self.layer_config)):
-            in_ch = out_ch or self.channel_config[0]
-            out_ch = self.channel_config[i]
+        _block = nn.ModuleList()
+
+        # base layer
+        _block.append(
+            SimpleConvLayer(
+                in_channels=input_ch,
+                out_channels=channel_config[0],
+                activation=init_activation,
+                normalization=init_normalization,
+                kernel_size=init_kernel_size,
+                stride=init_stride,
+                **kwargs,
+            )
+        )
+        for i in range(len(layer_config)):
+            in_ch = out_ch or channel_config[0]
+            out_ch = channel_config[i]
             kwargs = {
                 "in_channels": in_ch,
                 "out_channels": out_ch,
-                "stride": self.stride_config[i],
-                "n_layers": self.layer_config[i],
-                # "dilation": self.dilation_config[i],
-                "base_config": self.base_config,
+                "stride": stride_config[i],
+                "n_layers": layer_config[i],
+                # "dilation": dilation_config[i],
+                "base_config": base_config,
             }
 
-            self.block.append(GenericBlock(**kwargs))
+            _block.append(GenericBlock(**kwargs))
+        return _block
