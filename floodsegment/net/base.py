b6a14c7c89b457f4b8d2144ce356ea0cad27c9df
Neeth Kunnath <neeth.xavier@gmail.com>
2024-03-27 19:02:13 -0400

    add net.base

---
+++ floodsegment/net/base.py
@@ -0,0 +1,41 @@
+class _Buildable(nn.Module):
+    def __init__(self, **kwargs):
+        super(__class__, self).__init__()
+        self.block = self._build(**kwargs)
+
+    def _build(self, **kwargs) -> nn.ModuleList:
+        raise NotImplementedError(f"Please implement this fucntion for your class ({self.__class__.__name__})")
+
+    def forward(self, x):
+        return self.block(x)
+
+    def __len__(self):
+        return len(self.block)
+
+    def __getitem__(self, i):
+        return self.block[i]
+
+
+class BaseModule(_Buildable):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        *,
+        activation: Dict[str, Any],
+        normalization: Dict[str, Any],
+        **kwargs,
+    ):
+        _activation = build_object(**activation)
+        _normalization = build_object(**normalization, params={"num_features": in_channels})
+        super(__class__, self).__init__(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            activation=_activation,
+            normalization=_normalization,
+            **kwargs,
+        )
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.activation = _activation
+        self.normalization = _normalization
--- floodsegment/net/module.py
+++ floodsegment/net/module.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 from floodsegment.net.utils import compute_padding
+from floodsegment.net.base import BaseModule
 from floodsegment.utils.builder import build_object
 
 from typing import Any, Dict
@@ -7,49 +8,6 @@ from typing import Any, Dict
 import logging
 
 logger = logging.getLogger(__name__)
-
-
-class _Buildable(nn.Module):
-    def __init__(self, **kwargs):
-        super(__class__, self).__init__()
-        self.block = self._build(**kwargs)
-
-    def _build(self, **kwargs) -> nn.ModuleList:
-        raise NotImplementedError(f"Please implement this fucntion for your class ({self.__class__.__name__})")
-
-    def forward(self, x):
-        return self.block(x)
-
-    def __len__(self):
-        return len(self.block)
-
-    def __getitem__(self, i):
-        return self.block[i]
-
-
-class BaseModule(_Buildable):
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        *,
-        activation: Dict[str, Any],
-        normalization: Dict[str, Any],
-        **kwargs,
-    ):
-        _activation = build_object(**activation)
-        _normalization = build_object(**normalization, params={"num_features": in_channels})
-        super(__class__, self).__init__(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            activation=_activation,
-            normalization=_normalization,
-            **kwargs,
-        )
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.activation = _activation
-        self.normalization = _normalization
 
 
 class SimpleConvLayer(BaseModule):
