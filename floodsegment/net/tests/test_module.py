ea09f58d1f62968a8e67d8783012088239fb57d2
Neeth Kunnath <neeth.xavier@gmail.com>
2024-03-27 18:59:11 -0400

    better control over number of tests

--- floodsegment/net/tests/test_module.py
+++ floodsegment/net/tests/test_module.py
@@ -5,12 +5,15 @@ from floodsegment.utils.testers import check_module
 from floodsegment.net.module import SimpleConvLayer, GenericBlock
 
 
-@pytest.mark.parametrize("in_channels", np.random.randint(1, 128, size=3))
-@pytest.mark.parametrize("out_channels", np.random.randint(1, 128, size=3))
+NUM_TEST_VALS = 2
+
+
+@pytest.mark.parametrize("in_channels", np.random.randint(1, 128, size=NUM_TEST_VALS))
+@pytest.mark.parametrize("out_channels", np.random.randint(1, 128, size=NUM_TEST_VALS))
 @pytest.mark.parametrize("activation", [{"name": "torch.nn.ReLU"}])
 @pytest.mark.parametrize("normalization", [{"name": "torch.nn.BatchNorm2d"}])
-@pytest.mark.parametrize("kernel_size", 2 * np.random.randint(1, 128, size=3) + 1)
-@pytest.mark.parametrize("dilation", np.random.randint(1, 128, size=3))
+@pytest.mark.parametrize("kernel_size", 2 * np.random.randint(1, 128, size=NUM_TEST_VALS) + 1)
+@pytest.mark.parametrize("dilation", np.random.randint(1, 128, size=NUM_TEST_VALS))
 @pytest.mark.parametrize("kwargs", [{}])
 def test_SimpleConvLayer(in_channels, out_channels, activation, normalization, kernel_size, dilation, kwargs):
     c_layer = SimpleConvLayer(
@@ -35,7 +38,7 @@ def test_SimpleConvLayer(in_channels, out_channels, activation, normalization, k
     )
 
 
-@pytest.mark.parametrize("kernel_size", 2 * np.random.randint(1, 128, size=3))
+@pytest.mark.parametrize("kernel_size", 2 * np.random.randint(1, 128, size=NUM_TEST_VALS))
 def test_SimpleConvLayer_fail(kernel_size):
     in_channels = np.random.randint(1, 128)
     out_channels = np.random.randint(1, 128)
@@ -55,10 +58,10 @@ def test_SimpleConvLayer_fail(kernel_size):
         )
 
 
-@pytest.mark.parametrize("in_channels", np.random.randint(1, 128, size=3))
-@pytest.mark.parametrize("out_channels", np.random.randint(1, 128, size=3))
-@pytest.mark.parametrize("stride", np.random.randint(1, 16, size=3))
-@pytest.mark.parametrize("n_layers", np.random.randint(1, 64, size=3))
+@pytest.mark.parametrize("in_channels", np.random.randint(1, 128, size=NUM_TEST_VALS))
+@pytest.mark.parametrize("out_channels", np.random.randint(1, 128, size=NUM_TEST_VALS))
+@pytest.mark.parametrize("stride", np.random.randint(1, 16, size=NUM_TEST_VALS))
+@pytest.mark.parametrize("n_layers", np.random.randint(1, 64, size=NUM_TEST_VALS))
 def test_GenericBlock(in_channels, out_channels, stride, n_layers):
     base_config = {
         "name": "floodsegment.net.module.SimpleConvLayer",
